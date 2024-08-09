cc2ff20d00badb710d3e5b19067e0a7ec9b3a7c4
Abuzar Khan <abuzarkhan1242@gmail.com>
2024-08-09 13:57:41 +0500

    new routes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Abuzar-Portfolio/',
-})
+  base: '/Abuzar-Portfolio/' // Ensure this is set to your deployment path
+});
