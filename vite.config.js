788b401da40f247812df6f09037e6c59cb62b5dd
Abuzar Khan <abuzarkhan1242@gmail.com>
2024-08-09 12:43:26 +0500

    little changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Abuzar-Portfolio/"
+  base: "/", // or remove base option if it's the root
 })
