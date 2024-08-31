8e7e07ad796b2b6160e8c55cf872c6ff582fbc59
Abuzar Khan <abuzarkhan1242@gmail.com>
2024-08-31 16:02:35 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Abuzar/', // This should exactly match your repository name
+  base: '/Abuzar/', // Make sure this matches your repo name
   build: {
     outDir: 'dist',
   },
-})
+});
