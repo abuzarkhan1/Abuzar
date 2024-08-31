7a3f28b6a1bb61f3deb382767e5f3490ec909cc2
Abuzar Khan <abuzarkhan1242@gmail.com>
2024-08-31 14:53:28 +0500

    updated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,15 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        fontFamily: {
-            oswald: "Oswald, sans-serif",
+      fontFamily: {
+        oswald: "Oswald, sans-serif",
+      },
+      extend: {
+        transitionDuration: {
+          2500: "2500ms",
         },
-        extend: {
-            transitionDuration: {
-                2500: "2500ms",
-            },
-        },
+      },
     },
     plugins: [],
-};
+  };
+  
